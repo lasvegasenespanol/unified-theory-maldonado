7e457bad84fd9f07dd5ba333c392da668ef922f0
lasvegasenespanol <48595941+lasvegasenespanol@users.noreply.github.com>
2025-08-14 16:37:35 -0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('click', (e)=>{
+  const a = e.target.closest('a[href^="#"]');
+  if(!a) return;
+  e.preventDefault();
+  document.querySelector(a.getAttribute('href'))?.scrollIntoView({behavior:'smooth'});
+});
